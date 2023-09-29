34568849c555ee8f96c60f5d7da85eae29a5efb9
David <dalaeer@gmail.com>
2023-09-28 22:22:38 -0400

    updated libcplot/expr_parsing

---
+++ emcc-build/CMakeFiles/CPlot.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPlot.dir/imgui/backends/imgui_impl_sdl2.cpp.o"
+  "CMakeFiles/CPlot.dir/imgui/backends/imgui_impl_sdl2.cpp.o.d"
+  "CMakeFiles/CPlot.dir/imgui/backends/imgui_impl_sdlrenderer2.cpp.o"
+  "CMakeFiles/CPlot.dir/imgui/backends/imgui_impl_sdlrenderer2.cpp.o.d"
+  "CMakeFiles/CPlot.dir/imgui/imgui.cpp.o"
+  "CMakeFiles/CPlot.dir/imgui/imgui.cpp.o.d"
+  "CMakeFiles/CPlot.dir/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/CPlot.dir/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/CPlot.dir/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/CPlot.dir/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/CPlot.dir/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/CPlot.dir/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/CPlot.dir/libcplot/src/libcplot.cpp.o"
+  "CMakeFiles/CPlot.dir/libcplot/src/libcplot.cpp.o.d"
+  "CMakeFiles/CPlot.dir/libcplot/src/toojpeg.cpp.o"
+  "CMakeFiles/CPlot.dir/libcplot/src/toojpeg.cpp.o.d"
+  "CMakeFiles/CPlot.dir/main.cpp.o"
+  "CMakeFiles/CPlot.dir/main.cpp.o.d"
+  "CPlot.js"
+  "CPlot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPlot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -81,23 +81,7 @@ int main(int, char**)
     // Setup Platform/Renderer backends
     ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
     ImGui_ImplSDLRenderer2_Init(renderer);
-/*
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);  //windows font :(
-    io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != nullptr);
-*/
+
 
     auto font_calibri = io.Fonts->AddFontFromFileTTF("../assets/fonts/calibri.ttf", 16.0f);
     assert(font_calibri);
@@ -121,7 +105,7 @@ int main(int, char**)
     auto render = [&]()
     {
         try{
-            image.plot_complex_func(std::string(text_input), static_cast<double>(maxval), false, (multithreading ? std::thread::hardware_concurrency() : 1));
+            image.plot_complex_func(std::string(text_input), static_cast<double>(maxval), false, (multithreading ? static_cast<int>(std::thread::hardware_concurrency()) : 1));
         }
         catch(std::invalid_argument& e)
         {
